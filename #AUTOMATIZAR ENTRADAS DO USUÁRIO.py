705476bf28405e74efe5e11cb15588fd5fc560f6
Docinho_azedo_ <mcpassossouza@gmail.com>
2026-08-24 16:41:12 -0300

    Add files via upload

---
+++ #AUTOMATIZAR ENTRADAS DO USUÁRIO.py
@@ -0,0 +1,12 @@
+#AUTOMATIZAR ENTRADAS DO USUÁRIO
+quantidade = [1,2,3, 4, 5]
+for i in range(len(quantidade)):
+    entrada = int(input("Entre com os dados: "))
+
+
+
+quantidade = int(input("Entre com a quantidade: "))
+for i in range(quantidade):
+    entrada = int(input("Entre com os dados: "))
+    print(f"Entrada: ",{entrada})
+
