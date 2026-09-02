5a8a8e9f96c6a438d199031890bcaa0f38678891
Docinho_azedo_ <mcpassossouza@gmail.com>
2026-09-02 16:41:03 -0300

    Add files via upload

---
+++ consumo mensal.py
@@ -0,0 +1,27 @@
+total = 0
+maior = 0
+menor = 0
+mes_maior = 0
+
+for mes in range(1,13): # Vai do mês 1 ao 12
+    consumo_mensal = float(input(f"Digite o consumo do mês: {mes}"))
+
+    total = total + consumo_mensal
+
+    if mes == 1:
+        maior = consumo_mensal
+        menor = consumo_mensal
+        mes_maior = mes
+    else:
+        if consumo_mensal > maior:
+            maior = consumo_mensal
+            mes_maior = mes
+        if consumo_mensal < menor: # IF duplo para serem executados ao mesmo tempo
+            menor = consumo_mensal
+
+media =  total/12
+
+print("Relatório de gastos mensais")
+print(f"Consumo total: {total}")
+print(f"A média de gastos foi: {media}")
+print(f"O maior consumo foi: {maior} e o menor: {menor}")
