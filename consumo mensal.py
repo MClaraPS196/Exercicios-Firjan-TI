9732c278c33ec5bbeeb428fdfd32900cc31fb43c
Docinho_azedo_ <mcpassossouza@gmail.com>
2026-09-03 13:27:21 -0300

    Add files via upload

--- consumo mensal.py
+++ consumo mensal.py
@@ -1,27 +1,33 @@
-total = 0
-maior = 0
-menor = 0
-mes_maior = 0
 
-for mes in range(1,13): # Vai do mês 1 ao 12
-    consumo_mensal = float(input(f"Digite o consumo do mês: {mes}"))
+def calcular_consumo():
 
-    total = total + consumo_mensal
+    total = 0
+    maior = 0
+    menor = 0
+    mes_maior = 0
 
-    if mes == 1:
-        maior = consumo_mensal
-        menor = consumo_mensal
-        mes_maior = mes
-    else:
-        if consumo_mensal > maior:
+    for mes in range(1,13): # Vai do mês 1 ao 12
+        consumo_mensal = float(input(f"Digite o consumo do mês: {mes}"))
+
+        total = total + consumo_mensal
+
+        if mes == 1:
             maior = consumo_mensal
-            mes_maior = mes
-        if consumo_mensal < menor: # IF duplo para serem executados ao mesmo tempo
             menor = consumo_mensal
+            mes_maior = mes
+        else:
+            if consumo_mensal > maior:
+                maior = consumo_mensal
+                mes_maior = mes
+            if consumo_mensal < menor: # IF duplo para serem executados ao mesmo tempo
+                menor = consumo_mensal
 
-media =  total/12
+    media =  total/12
 
-print("Relatório de gastos mensais")
-print(f"Consumo total: {total}")
-print(f"A média de gastos foi: {media}")
-print(f"O maior consumo foi: {maior} e o menor: {menor}")
+    print("\n--- RESULTADO ---")
+    print("Relatório de gastos mensais")
+    print(f"Consumo total: {total}")
+    print(f"A média de gastos foi: {media:.2f}")
+    print(f"O maior consumo foi: {maior} e o menor: {menor}")
+
+calcular_consumo()
